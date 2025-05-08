488fefcf48a422c3df03d51f1e118825b758d0ce
mpv <193361506+mp-v99@users.noreply.github.com>
2025-05-07 23:08:14 -0600

    Finish adding extra points such as:
    Defensive logic to prevent bugs when pressing
    equals button prematurely. The event listener only
    responds if the secondOperand exists.
    
    Keyboard supoprt works for every key: arithmetic
    functions, number functions, clearing and deleting.
    
    For HTML, the ID of the decimal was changed to '.'
    in order for the code recognize it based on the
    e.key
    
    I tried with e.code but since there are two keys
    that write the period in the keyboard, it overcomplicated
    the conditional.

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// Math functions
-
-const add = function(a, b) {
-    return Math.round((parseFloat(a)+ parseFloat(b)) * 10) / 10;
-  };
-  
-const subtract = function(a, b) {
-    return Math.round((parseFloat(a)- parseFloat(b)) * 10) / 10;
-};
-
-const divide = function(a, b) {
-    if (b == 0) {
-        return `Well, if you're dividing by zero, you're not doing math — you're performing an act of metaphysical rebellion against reality itself.`
-    }
-    return Math.round((parseFloat(a)/ parseFloat(b)) * 10) / 10;
-};
-
-const multiply = function(a, b) {
-    return Math.round((parseFloat(a)* parseFloat(b)) * 10) / 10;
-};
-
 // Buttons Selectors
 
 const numberButtons = document.querySelectorAll('.number_button');
@@ -28,46 +7,23 @@ const clearButton = document.querySelector('#btn-clear');
 const backSpaceButton = document.querySelector('#btn-backspace');
 const displayResult = document.querySelector('#result');
 
-let firstOperand = '';
-let secondOperand = '';
-let operator = '';
+
 let isDisplayEmpty = true;
 let isDecimalEmpty = true;
+let isJokeMessage = false;
+
+let operation = {
+    firstOperand: '',
+    secondOperand: '',
+    operator: '',
+};
+
+// Number Buttons Event Listener
 
 
-// Number Buttons Evenet Listener
-
-
-numberButtons.forEach((button) => {
+numberButtons.forEach((button) => { 
     button.addEventListener('click', (e) => {
-        if (isDisplayEmpty && !operator) {
-            displayResult.textContent = '';
-            firstOperand = ''
-            isDisplayEmpty = false;
-        }
-        if (!operator) { // This condition prevents from adding more digits after having selected an operator
-            if(e.target.id === 'btn-dot' && isDecimalEmpty && firstOperand.length >= 1) {
-                firstOperand += button.textContent
-                displayResult.textContent += button.textContent;
-                isDecimalEmpty = false;
-            }
-            else if(firstOperand.length <= 11 && e.target.id != 'btn-dot') {  // This condition limits the length of the operand
-                firstOperand += button.textContent
-                displayResult.textContent += button.textContent;
-            }
-        }
-        else {
-            if(e.target.id === 'btn-dot' && isDecimalEmpty && secondOperand.length >= 1) {
-                secondOperand += button.textContent
-                displayResult.textContent += button.textContent;
-                isDecimalEmpty = false;
-            }
-            if(secondOperand.length <= 11 && e.target.id != 'btn-dot') {
-                secondOperand += button.textContent;
-                displayResult.textContent += button.textContent;
-                console.log(`${firstOperand} ${operator} ${secondOperand}`)
-            }
-        }
+       numberButtonsFunction(button.textContent, e.target.id);
     }) 
 })
 
@@ -75,12 +31,7 @@ numberButtons.forEach((button) => {
 
 operatorButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        if (!operator && firstOperand) {
-            operator += button.textContent;
-            displayResult.textContent += operator;
-            isDecimalEmpty = true;
-            console.log(`operator: ${typeof firstOperand}`)
-        }
+        operatorButtonsFunction(button.textContent);
     })
 });
 
@@ -88,68 +39,219 @@ operatorButtons.forEach((button) => {
 
 
 equalButton.addEventListener('click', () => {
-    if (firstOperand && secondOperand) {
-        switch(operator) {
-            case '÷': 
-                displayResult.textContent = divide(firstOperand, secondOperand)
-                firstOperand = divide(firstOperand, secondOperand)
-            break;
-            case 'x':
-                displayResult.textContent = multiply(firstOperand, secondOperand)
-                firstOperand = multiply(firstOperand, secondOperand)
-            break;
-            case '+':
-                displayResult.textContent = add(firstOperand, secondOperand)
-                firstOperand = add(firstOperand, secondOperand)
-            break;
-            case '-':
-                displayResult.textContent = subtract(firstOperand, secondOperand)
-                firstOperand = subtract(firstOperand, secondOperand)
-                console.log(firstOperand)
-
-            break;
-        }
-    }
-    secondOperand = '';
-    operator = '';
-    isDisplayEmpty = true;
-    isDecimal = true;
+    equalsFunction();
 });
 
 // Clear Button Event Listener 
 
 clearButton.addEventListener('click', () => {
-    firstOperand = '';
-    secondOperand = '';
-    operator = '';
-    isDisplayEmpty = true;
-    isDecimal = true;
-    displayResult.textContent = '';
+    clearFunction();
 })
 
 // Backspace Button Event Listener
 
 backSpaceButton.addEventListener('click', () => {
-        if (secondOperand.length >= 1) {
-            secondOperand = secondOperand.slice(0, -1);
-            displayResult.textContent = displayResult.textContent.slice(0,-1);
-        }
-        else if (operator.length == 1) {
-            operator = operator.slice(0, -1);
-            displayResult.textContent = displayResult.textContent.slice(0,-1);
-        }
-        else if (typeof firstOperand === 'number') {
-            firstOperand = '';
-            secondOperand = '';
-            operator = '';
-            isDisplayEmpty = true;
-            isDecimalTrue = true;
-            displayResult.textContent = '';
-        }
-        else if (firstOperand.length >= 1) {
-            firstOperand = firstOperand.slice(0, -1);
-            console.log(firstOperand)
-            displayResult.textContent = displayResult.textContent.slice(0,-1);
+   backspaceFunction();  
+});
 
-        }
+// Keyboard Event Listeners
+
+document.addEventListener('keydown', (e) => {
+    console.log(e.key)
+    switch(e.key) {
+        case 'Enter':
+            equalsFunction();
+        break;
+        case 'Escape':
+            clearFunction();
+        break;
+        case 'Backspace':
+            backspaceFunction();
+        break;
+        case '/':
+            operatorButtonsFunction('÷');
+        break;
+        case '*':
+            operatorButtonsFunction('x');
+        break;
+        case '+':
+            operatorButtonsFunction(e.key);
+        break;
+        case '-':
+            operatorButtonsFunction(e.key);
+        break;    
+        case '0':
+            numberButtonsFunction(e.key, e.key);
+        break;
+        case '1':
+            numberButtonsFunction(e.key, e.key);
+        break;
+        case '2':
+            numberButtonsFunction(e.key, e.key);
+        break;
+        case '3':
+            numberButtonsFunction(e.key, e.key);
+        break;
+        case '4':
+            numberButtonsFunction(e.key, e.key);
+        break;
+        case '5':
+            numberButtonsFunction(e.key, e.key);
+        break;
+        case '6':
+            numberButtonsFunction(e.key, e.key);
+        break;
+        case '7':
+            numberButtonsFunction(e.key, e.key);
+        break;
+        case '8':
+            numberButtonsFunction(e.key, e.key);
+        break;
+        case '9':
+            numberButtonsFunction(e.key, e.key);
+        break;
+        case '.':
+            numberButtonsFunction(e.key, e.key);
+        break;
+    }
 })
+
+// Operate function
+
+const operate = function(firstOperand, secondOperand, operator) {
+    switch(operator) {
+        case '÷': 
+            displayResult.textContent = divide(firstOperand, secondOperand)
+            operation.firstOperand = divide(firstOperand, secondOperand) // Reassign the result to the first operand to do chain evaluation
+        break;
+        case 'x':
+            displayResult.textContent = multiply(firstOperand, secondOperand)
+            operation.firstOperand = multiply(firstOperand, secondOperand)
+        break;
+        case '+':
+            displayResult.textContent = add(firstOperand, secondOperand)
+            operation.firstOperand = add(firstOperand, secondOperand)
+        break;
+        case '-':
+            displayResult.textContent = subtract(firstOperand, secondOperand)
+            operation.firstOperand = subtract(firstOperand, secondOperand)
+        break;
+    }
+}
+
+// Math functions
+
+const add = function(a, b) {
+    return Math.round((parseFloat(a)+ parseFloat(b)) * 1000) / 1000;
+  };
+  
+const subtract = function(a, b) {
+    return Math.round((parseFloat(a)- parseFloat(b)) * 1000) / 1000;
+};
+
+const divide = function(a, b) {
+    if (b == 0) {
+        operation.firstOperand = ''
+        operation.secondOperand = '';
+        operation.operator = '';
+        isJokeMessage = true;
+        return `Well, if you're dividing by zero, you're not doing math — you're performing an act of metaphysical rebellion against reality itself.`
+    }
+    return Math.round((parseFloat(a)/ parseFloat(b)) * 1000) / 1000;
+};
+
+const multiply = function(a, b) {
+    return Math.round((parseFloat(a)* parseFloat(b)) * 1000) / 1000;
+};
+
+
+// Listener Buttons Functions:
+
+const numberButtonsFunction = function(number, e) {
+    if (isDisplayEmpty && !operation.operator) { // This condition allows to reset in case the user types a new number after getting a result
+        displayResult.textContent = '';
+        operation.firstOperand = '';
+        isDisplayEmpty = false;
+        isJokeMessage = false;
+    }
+    if (!operation.operator) { // This condition prevents from adding more digits after having selected an operator
+        if(e === '.' && isDecimalEmpty && operation.firstOperand.length >= 1) { // Adds the dot only if the dot hasn't been used and there's at least one digit in the operand
+            operation.firstOperand += number
+            displayResult.textContent += number;
+            isDecimalEmpty = false;
+        }
+        else if(operation.firstOperand.length <= 11 && e !== '.') {  // This condition limits the length of the operand
+            operation.firstOperand += number
+            displayResult.textContent += number;
+        }
+    }
+    else {
+        if(e === '.' && isDecimalEmpty && operation.secondOperand.length >= 1) {
+            operation.secondOperand += number
+            displayResult.textContent += number;
+            isDecimalEmpty = false;
+        }
+        else if(operation.secondOperand.length <= 11 && e !== '.') {
+            operation.secondOperand += number;
+            displayResult.textContent += number;
+        }
+    }
+};
+
+const operatorButtonsFunction = function(operator) {
+    if (operation.firstOperand && !isJokeMessage) { // prevents behavior where a new operation can be started when joke message is displayed
+        operation.operator = '';
+        operation.operator = operator;
+        displayResult.textContent = operation.firstOperand + operator;
+        isDecimalEmpty = true; // This allows to add a decimal to the second operand
+    }
+}
+
+const equalsFunction = function() {
+    if (operation.firstOperand  && operation.secondOperand) {
+        console.table(operation);
+        operate(operation.firstOperand, operation.secondOperand, operation.operator);
+        isDisplayEmpty = true;
+        operation.secondOperand = '';
+        operation.operator = '';
+        isDecimalEmpty = true;
+        isOperatorEmpty = true;
+        
+    }
+}
+
+const clearFunction = function() {
+    operation.firstOperand = '';
+    operation.secondOperand = '';
+    operation.operator = '';
+    isDisplayEmpty = true;
+    isDecimalEmpty = true;
+    isOperatorEmpty = true;
+    displayResult.textContent = '';
+};
+
+const backspaceFunction = function() {
+    if (operation.secondOperand.length >= 1) {
+        operation.secondOperand = operation.secondOperand.slice(0, -1);
+        displayResult.textContent = displayResult.textContent.slice(0,-1);
+    }
+    else if (operation.operator.length == 1) {
+        operation.operator = operation.operator.slice(0, -1);
+        isOperatorEmpty = true;
+        displayResult.textContent = displayResult.textContent.slice(0,-1); 
+    }
+    else if (typeof operation.firstOperand === 'number' || isJokeMessage) { // Clear everything in case the first operand                                                                
+        operation.firstOperand = '';                                       // is the result of a previous calculation or is 
+        operation.secondOperand = '';                                     // the mocking message when dividing by zero
+        operation.operator = '';
+        isDisplayEmpty = true;
+        isDecimalEmpty = true;
+        isOperatorEmpty = true;
+        isJokeMessage = false;
+        displayResult.textContent = '';
+    }
+    else if (operation.firstOperand.length >= 1) {
+        operation.firstOperand = operation.firstOperand.slice(0, -1);
+        displayResult.textContent = displayResult.textContent.slice(0,-1);
+    }
+};
